3735638b6e9eb55c3f6cf0f364215ff07607e6f5
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2024-05-21 16:10:14 +0300

    Task 2

---
+++ task2/task2.js
@@ -0,0 +1,67 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var posts = [
+    {
+        id: '62e69d5a5458aac0ed320b35',
+        title: 'id labore ex et quam laborum',
+        body: 'laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibusdolor quam autem quasireiciendis et nam sapiente accusantium',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b1c',
+        title: 'quo vero reiciendis velit similique earum',
+        body: 'est natus enim nihil est dolore omnis voluptatem numquamet omnis occaecati quod ullam at voluptatem error expedita pariaturnihil sint nostrum voluptatem reiciendis et',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b32',
+        title: 'odio adipisci rerum aut animi',
+        body: 'quia molestiae reprehenderit quasi aspernaturaut expedita occaecati aliquam eveniet laudantiumomnis quibusdam delectus saepe quia accusamus maiores nam estcum et ducimus et vero voluptates excepturi deleniti ratione',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b39',
+        title: 'alias odio sit',
+        body: 'non et atqueoccaecati deserunt quas accusantium unde odit nobis qui voluptatemquia voluptas consequuntur itaque doloret qui rerum deleniti ut occaecati',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b53',
+        title: 'vero eaque aliquid doloribus et culpa',
+        body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b19',
+        title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
+        body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in',
+    },
+    {
+        id: '62e69d5a5458aac0ed320b25',
+        title: 'repellat consequatur praesentium vel minus molestias voluptatum',
+        body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor',
+    },
+];
+var normalizeData = function (unnormalizedData) {
+    var result = unnormalizedData.reduce(function (acc, cur) {
+        var _a;
+        return __assign(__assign({}, acc), { byId: (_a = {},
+                _a[cur.id] = cur,
+                _a), allIds: __spreadArray(__spreadArray([], (acc.allIds || []), true), [cur.id], false) });
+    }, {});
+    return result;
+};
+console.log(normalizeData(posts));
